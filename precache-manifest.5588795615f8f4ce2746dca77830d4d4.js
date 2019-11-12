23e42842b4355da982c6a00f519169bc0af5d387
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-12 13:35:20 +0000

    Deploy willnode/trunojoyo-assets to github.com/willnode/trunojoyo-assets.git:gh-pages

---
+++ precache-manifest.5588795615f8f4ce2746dca77830d4d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d5188c7d7699afcd3b3c3bc9a90669",
+    "url": "/trunojoyo-assets/index.html"
+  },
+  {
+    "revision": "0556caf4534fbb8d66b7",
+    "url": "/trunojoyo-assets/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "8c22bd213eb5da614215",
+    "url": "/trunojoyo-assets/static/css/main.ab0c65ae.chunk.css"
+  },
+  {
+    "revision": "0556caf4534fbb8d66b7",
+    "url": "/trunojoyo-assets/static/js/2.9e41a25a.chunk.js"
+  },
+  {
+    "revision": "8c22bd213eb5da614215",
+    "url": "/trunojoyo-assets/static/js/main.e348ea84.chunk.js"
+  },
+  {
+    "revision": "f0e85be6bc6a0fc7ef4d",
+    "url": "/trunojoyo-assets/static/js/runtime-main.e8bdb429.js"
+  },
+  {
+    "revision": "08dfa457aa8fd148bda9ebc4d13a2bef",
+    "url": "/trunojoyo-assets/static/media/graha.08dfa457.svg"
+  },
+  {
+    "revision": "f49f769f0ee5702e463529482acd2ec9",
+    "url": "/trunojoyo-assets/static/media/trunojoyo-bow.f49f769f.svg"
+  },
+  {
+    "revision": "15e865dfe26e1036c5a4578c72cd8e55",
+    "url": "/trunojoyo-assets/static/media/trunojoyo-wob.15e865df.svg"
+  },
+  {
+    "revision": "3a10a82393ff5ec490a59f99f9c53dc0",
+    "url": "/trunojoyo-assets/static/media/trunojoyo.3a10a823.svg"
+  },
+  {
+    "revision": "bfd008bd76fff8d71735c322dc7f63c9",
+    "url": "/trunojoyo-assets/static/media/trunojoyo.bfd008bd.png"
+  }
+]);
